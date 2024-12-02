18f1cb8ad88e8ee609687aa25d2c08254f5856a8
mu9 <1457993902@qq.com>
2024-12-02 21:59:29 +0800

    fslab

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -330,6 +330,24 @@ sys_open(void)
     return -1;
   }
 
+  int i = 0;
+  while (ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+    readi(ip, 0, (uint64)path, 0, MAXPATH);
+    iunlockput(ip);
+    if ((ip = namei(path)) == 0)
+    {
+      end_op();
+      return -1;
+    }
+    ilock(ip);
+    if (++i > 10)
+    {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+  } 
+
   if(ip->type == T_DEVICE){
     f->type = FD_DEVICE;
     f->major = ip->major;
@@ -482,5 +500,35 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH];
+  char path[MAXPATH];
+  struct inode *ip;
+
+  if (argstr(0, target, MAXPATH) < 0)
+    return -1;
+  if (argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0)
+  {
+    if ((ip = namei(path)) == 0)
+    {
+      end_op();
+      return -1;
+    }
+    ilock(ip);
+  }
+  writei(ip, 0, (uint64)target, 0, MAXPATH);
+  iunlockput(ip);
+
+  end_op();
   return 0;
 }
